1687a8a73345640a404582044dccd7f94c5ba5ea
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2021-09-04 00:55:35 +0100

    implement all routes and modularize models and function calls

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
+const movieRoute = require("./routes/movie");
+const listRoute = require("./routes/list");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
+
+app.listen(8080, () => {
+  console.log("Backend server is running!");
+});
